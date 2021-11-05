c49b1fe774f115bee55e65835e07bbc2bf73f038
Giovanni (Gianni) Bernardo <settorezero@gmail.com>
2021-11-05 17:39:33 +0100

    changed function for drawing pointer
    - added a structure for setting pointer characteristics
    - removed second function for drawing second pointer: used same function by passing the pointer to the above structure
    - changed font for writing value so you can also print letters and symbols
    - changed instrument position for using the whole space
    - added a label under the value if you want to write measure unit
    
    Co-Authored-By: Paolo Loberto <84834981+mrloba81@users.noreply.github.com>

--- analog_gauge/analog_gauge.ino
+++ analog_gauge/analog_gauge.ino
@@ -1,5 +1,5 @@
 /*
- * Analog gauge example by @cyb3rn0id
+ * Analog gauge example by @cyb3rn0id and @mrloba81
  *
  * This example started from a work of BODMER
  * taken from: https://www.instructables.com/Arduino-sketch-for-a-retro-analogue-meter-graphic-/
@@ -21,35 +21,79 @@
 #include "Ucglib.h"
 
 // ST7735 display to Arduino Nano (or Nano Every) connections:
-// CLK : 13
-// SDA : 11
-// RS  :  9 (is called "CD" from the library. RS=register select, CD=command/data => is the same thing!)
-// CS  : 10
-// RST :  8
-// notice: on some displays, RS is marked as 'A0'
+// CLK      : 13
+// SDA      : 11
+// RS or A0 :  9 (is called "CD" from the library. RS=register select, CD=command/data => is the same thing!)
+// CS       : 10
+// RST      :  8
 
 // display instance using software SPI (first) or hardware SPI (second)
 //Ucglib_ST7735_18x128x160_SWSPI ucg(/*sclk=*/ 13, /*data=*/ 11, /*cd=*/ 9, /*cs=*/ 10, /*reset=*/ 8);
 //Ucglib_ST7735_18x128x160_HWSPI ucg(/*cd=*/ 9, /*cs=*/ 10, /*reset=*/ 8);
 Ucglib_ST7735_18x128x160_HWSPI ucg(9, 10, 8);
 
-// constants used for the first gauge
-#define G1_X 60
-#define G1_Y 62
+/// constants used for the first gauge
+#define G1_X 56 // X coordinates of the center of the circle
+#define G1_Y 64
 #define G1_ARC 120
 #define G1_RADIUS 30
+#define G1_SCALEMIN 0
+#define G1_SCALEMAX 100
 
 // constants used for the second gauge
-#define G2_X 60
-#define G2_Y 124
+#define G2_X 56
+#define G2_Y 127
 #define G2_ARC 120
 #define G2_RADIUS 30
+#define G2_SCALEMIN -50
+#define G2_SCALEMAX 50
 
-#define c 0.0174532925F // pi/180 (for grad to rad conversion)
+#define K 0.0174532925F // pi/180 (for grad to rad conversion)
+
+// structure used for handling more than 1 gauge using same function
+struct DrawContext 
+  {
+  float old_val;
+  float ltx;
+  uint16_t osx;
+  uint16_t osy;
+  bool first_start;
+  uint16_t gaugeposX;
+  uint16_t gaugeposY;
+  uint8_t gaugeArc;
+  uint8_t gaugeRadius;
+  float scaleMin;
+  float scaleMax;
+  } pointer1, pointer2; // I've defined 2 different values for 2 different gauges
 
 void setup(void)
   {
-  delay(1000);
+  // values for pointer of first gauge
+  pointer1.old_val = -99999;
+  pointer1.ltx = 0; 
+  pointer1.osx = 0;
+  pointer1.osy = 0;
+  pointer1.first_start = true;
+  pointer1.gaugeposX=G1_X;
+  pointer1.gaugeposY=G1_Y;
+  pointer1.gaugeArc=G1_ARC;
+  pointer1.gaugeRadius=G1_RADIUS;
+  pointer1.scaleMin=G1_SCALEMIN;
+  pointer1.scaleMax=G1_SCALEMAX;
+  
+  pointer2.old_val = -99999;
+  pointer2.ltx = 0; 
+  pointer2.osx = 0;
+  pointer2.osy = 0;
+  pointer2.first_start = true;
+  pointer2.gaugeposX=G2_X;
+  pointer2.gaugeposY=G2_Y;
+  pointer2.gaugeArc=G2_ARC;
+  pointer2.gaugeRadius=G2_RADIUS;
+  pointer2.scaleMin=G2_SCALEMIN;
+  pointer2.scaleMax=G2_SCALEMAX;
+  
+  delay(400);
   ucg.begin(UCG_FONT_MODE_TRANSPARENT);
   ucg.clearScreen();
   ucg.setRotate90(); // display horizontal (landscape) with header on the left
@@ -66,45 +110,50 @@ void setup(void)
 
 void loop(void)
   {
-  static int16_t vo0=0;
   static int16_t vo1=0;
+  static int16_t vo2=0;
   static uint8_t i=0;
 
   // first trimmer mapped to 0/100
-  int16_t v0=map(analogRead(A0), 0, 1023, 0 ,100);
+  int16_t v1=map(analogRead(A0), 0, 1023, 0 ,100);
   // second trimmer mapped to -50/50
-  int16_t v1=map(analogRead(A1), 0, 1023, -50 ,50);
-  vo0+=v0;
+  int16_t v2=map(analogRead(A1), 0, 1023, -50 ,50);
   vo1+=v1;
+  vo2+=v2;
   i++;
   if (i==30)
     {
-    v0=vo0/30;
-    vo0=0;
-    v1=vo1/30;
-    vo1=0;  
+    v1=v1/30;
+    vo1=0;
+    v2=vo2/30;
+    vo2=0;  
     i=0;
-    
-    // drawPointer(uint8_t x, uint8_t y, uint8_t arc, uint8_t radius, int value, int minval, int maxval)
-	// since the drawpointer function uses static variables for keeping in memory where was the last pointer
-    // position for deleting it after a refresh, I've copied an equal second function for the second gauge
-    drawPointer(G1_X, G1_Y, G1_ARC, G1_RADIUS, v0, 0, 100);
-    drawPointer2(G2_X, G2_Y, G2_ARC, G2_RADIUS, v1, -50, 50);
-    
+
     // set font
     ucg.setColor(0, 0, 0, 0); // foreground color
     ucg.setColor(1, 255, 255, 255); // background color
     ucg.setFontMode(UCG_FONT_MODE_SOLID); // solid: background will painted
-    ucg.setFont(ucg_font_logisoso16_hn); // font (https://github.com/olikraus/ucglib/wiki/fontsize)
-
+    // use this font if you want ONLY numbers
+    //ucg.setFont(ucg_font_logisoso16_hn); // font (https://github.com/olikraus/ucglib/wiki/fontsize)
+    // use this font for numbers, letters, symbols
+    ucg.setFont(ucg_font_logisoso18_hr); // font (https://github.com/olikraus/ucglib/wiki/fontsize)
+    
     // write values
-    ucg.setPrintPos(G1_X+G1_RADIUS+35, G1_Y-23);
-    ucg.print(v0);
+    ucg.setPrintPos(G1_X + G1_RADIUS + 27, G1_Y - 33); // 23
+    ucg.print(v1, 1); // 1 decimal, not used in this case
     ucg.print("  ");
+    ucg.setPrintPos(G1_X + G1_RADIUS + 27, G1_Y - 10);
+    ucg.print("V1");
 
-    ucg.setPrintPos(G2_X+G2_RADIUS+35, G2_Y-23);
-    ucg.print(v1);
+    ucg.setPrintPos(G2_X + G2_RADIUS + 27, G2_Y - 33);
+    ucg.print(v2); // no decimals
     ucg.print("  ");
+    ucg.setPrintPos(G2_X + G2_RADIUS + 27, G2_Y - 10);
+    ucg.print("V2");
+    
+    // drawPointer(struct DrawContest pointer, float value)
+    drawPointer(pointer1, v1);
+    drawPointer(pointer2, v2);
     }
  }
 
@@ -117,16 +166,16 @@ void drawGauge(uint8_t x, uint8_t y, uint8_t arc, uint8_t radius, uint8_t stp, u
     {
     // Calculating coodinates of tick to draw
     // at this time will be used only for drawing the green/yellow/red zones
-    float sx = cos((i - 90) * c);
-    float sy = sin((i - 90) * c);
+    float sx = cos((i - 90) * K);
+    float sy = sin((i - 90) * K);
     uint16_t x0 = sx * (radius + tickl) + x;
     uint16_t y0 = sy * (radius + tickl) + y;
     uint16_t x1 = sx * radius + x;
     uint16_t y1 = sy * radius + y;
     
     // Coordinates of next tick for zone fill
-    float sx2 = cos((i + stp - 90) * c);
-    float sy2 = sin((i + stp - 90) * c);
+    float sx2 = cos((i + stp - 90) * K);
+    float sy2 = sin((i + stp - 90) * K);
     int x2 = sx2 * (radius + tickl) + x;
     int y2 = sy2 * (radius + tickl) + y;
     int x3 = sx2 * radius + x;
@@ -199,8 +248,8 @@ void drawGauge(uint8_t x, uint8_t y, uint8_t arc, uint8_t radius, uint8_t stp, u
     } // check if I must print labels
     
     // Now draw the arc of the scale
-    sx = cos((i + stp - 90) * c);
-    sy = sin((i + stp - 90) * c);
+    sx = cos((i + stp - 90) * K);
+    sy = sin((i + stp - 90) * K);
     x0 = sx * radius + x;
     y0 = sy * radius + y;
     ucg.setColor(0, 0, 0, 0);    
@@ -208,99 +257,48 @@ void drawGauge(uint8_t x, uint8_t y, uint8_t arc, uint8_t radius, uint8_t stp, u
   }
 }
 
-void drawPointer(uint8_t x, uint8_t y, uint8_t arc, uint8_t radius, float value, float minval, float maxval)
-  {
-  uint8_t nl=10; // pointer "length" (0=longest)
-  static float old_val=-999;
-  // quick exit if value to paint is the same
-  if (value==old_val) return;
-  old_val=value;
-  
-  static float ltx = 0; // Saved x coord of bottom of pointer
-  static uint16_t osx=0;
-  static uint16_t osy=0; 
-  static bool first_start=true;
-      
-  if (value<minval) value=minval; 
-  if (value>maxval) value=maxval;
+void drawPointer(DrawContext &ctx, float value)
+{
+  uint8_t nl = 10; // pointer "length" (0=longest)
+  // quick exit if value to paint is about the same for avoiding pointer flickering
+  //if ((value <= ctx.old_val + .05) && (value >= ctx.old_val - .05)) return;
+  if (value==ctx.old_val) return;
+  ctx.old_val = value;
 
-  float sdeg = lmap(value, minval, maxval, (-(arc/2)-90), ((arc/2)-90)); // Map value to angle 
+  if (value < ctx.scaleMin) value = ctx.scaleMin;
+  if (value > ctx.scaleMax) value = ctx.scaleMax;
+
+  float sdeg = lmap(value, ctx.scaleMin, ctx.scaleMax, (-(ctx.gaugeArc / 2) - 90), ((ctx.gaugeArc / 2) - 90)); // Map value to angle
+  
   // Calculate tip of pointer coords
-  float sx = cos(sdeg * c);
-  float sy = sin(sdeg * c);
+  float sx = cos(sdeg * K);
+  float sy = sin(sdeg * K);
 
   // Calculate x delta of needle start (does not start at pivot point)
-  float tx = tan((sdeg+90) * c);
-    
+  float tx = tan((sdeg + 90) * K);
+
   // Erase old needle image if not first time pointer is drawn
-  if (!first_start)
-      {
-      ucg.setColor(0, 255, 255, 255);
-      ucg.drawLine(x + nl * ltx - 1, y - nl, osx - 1, osy);
-      ucg.drawLine(x + nl * ltx, y - nl, osx, osy);
-      ucg.drawLine(x + nl * ltx + 1, y - nl, osx + 1, osy);
-      }
-  first_start=false;
-   
+  if (!ctx.first_start)
+    {
+    ucg.setColor(0, 255, 255, 255);
+    ucg.drawLine(ctx.gaugeposX + nl * ctx.ltx - 1, ctx.gaugeposY - nl, ctx.osx - 1, ctx.osy);
+    ucg.drawLine(ctx.gaugeposX + nl * ctx.ltx, ctx.gaugeposY - nl, ctx.osx, ctx.osy);
+    ucg.drawLine(ctx.gaugeposX + nl * ctx.ltx + 1, ctx.gaugeposY - nl, ctx.osx + 1, ctx.osy);
+    }
+  ctx.first_start = false;
+
   // Store new pointer end coords for next erase
-  ltx = tx;
-  osx = sx * (radius-2) + x;
-  osy = sy * (radius-2) + y;
-    
+  ctx.ltx = tx;
+  ctx.osx = sx * (ctx.gaugeRadius - 2) + ctx.gaugeposX;
+  ctx.osy = sy * (ctx.gaugeRadius - 2) + ctx.gaugeposY;
+
   // Draw the pointer in the new position
   // draws 3 lines to thicken needle
   ucg.setColor(0, 0, 0, 0);
-  ucg.drawLine(x + nl * ltx - 1, y - nl, osx - 1, osy);
-  ucg.drawLine(x + nl * ltx, y - nl, osx, osy);
-  ucg.drawLine(x + nl * ltx + 1, y - nl, osx + 1, osy);
-  }
-
-void drawPointer2(uint8_t x, uint8_t y, uint8_t arc, uint8_t radius, float value, float minval, float maxval)
-  {
-  uint8_t nl=10; // pointer "length" (0=longest)
-  static float old_val=-999;
-  // quick exit if value to paint is the same
-  if (value==old_val) return;
-  old_val=value;
-  
-  static float ltx = 0; // Saved x coord of bottom of pointer
-  static uint16_t osx=0;
-  static uint16_t osy=0; 
-  static bool first_start=true;
-      
-  if (value<minval) value=minval; 
-  if (value>maxval) value=maxval;
-
-  float sdeg = lmap(value, minval, maxval, (-(arc/2)-90), ((arc/2)-90)); // Map value to angle 
-  // Calculate tip of pointer coords
-  float sx = cos(sdeg * c);
-  float sy = sin(sdeg * c);
-
-  // Calculate x delta of needle start (does not start at pivot point)
-  float tx = tan((sdeg+90) * c);
-    
-  // Erase old needle image if not first time pointer is drawn
-  if (!first_start)
-      {
-      ucg.setColor(0, 255, 255, 255);
-      ucg.drawLine(x + nl * ltx - 1, y - nl, osx - 1, osy);
-      ucg.drawLine(x + nl * ltx, y - nl, osx, osy);
-      ucg.drawLine(x + nl * ltx + 1, y - nl, osx + 1, osy);
-      }
-  first_start=false;
-   
-  // Store new pointer end coords for next erase
-  ltx = tx;
-  osx = sx * (radius-2) + x;
-  osy = sy * (radius-2) + y;
-    
-  // Draw the pointer in the new position
-  // draws 3 lines to thicken needle
-  ucg.setColor(0, 0, 0, 0);
-  ucg.drawLine(x + nl * ltx - 1, y - nl, osx - 1, osy);
-  ucg.drawLine(x + nl * ltx, y - nl, osx, osy);
-  ucg.drawLine(x + nl * ltx + 1, y - nl, osx + 1, osy);
-  }
+  ucg.drawLine(ctx.gaugeposX + nl * ctx.ltx - 1, ctx.gaugeposY - nl, ctx.osx - 1, ctx.osy);
+  ucg.drawLine(ctx.gaugeposX + nl * ctx.ltx, ctx.gaugeposY - nl, ctx.osx, ctx.osy);
+  ucg.drawLine(ctx.gaugeposX + nl * ctx.ltx + 1, ctx.gaugeposY - nl, ctx.osx + 1, ctx.osy);
+}
 
 // Arduino "MAP" function but rewrited for using float numbers
 float lmap(float x, float in_min, float in_max, float out_min, float out_max) 
